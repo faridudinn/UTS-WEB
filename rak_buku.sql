0f5a3996d5088d150a057005d8bb9619bb3a9dc5
faridudinn <134308358+faridudinn@users.noreply.github.com>
2023-06-01 12:47:23 +0700

    Add files via upload

---
+++ rak_buku.sql
@@ -0,0 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 30, 2023 at 05:56 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rak_buku`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komik`
+--
+
+CREATE TABLE `komik` (
+  `id_buku` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `tanggal` date NOT NULL,
+  `judul_buku` varchar(30) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `file_buku` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `komik`
+--
+
+INSERT INTO `komik` (`id_buku`, `nama`, `tanggal`, `judul_buku`, `alamat`, `no_hp`, `jenis_kelamin`, `file_buku`) VALUES
+(32, 'Latifa', '2023-05-09', 'Boboiboy', 'Bangil', '098765432', 'Perempuan', 0x54756761732042492d206c6174696661207269736b612e646f6378),
+(33, 'Aini', '2023-05-10', 'barbie', 'Banyumas', '0976543222', 'Perempuan', 0x70656e6775696e2e7079);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `laporan`
+--
+
+CREATE TABLE `laporan` (
+  `id_buku` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `tanggal` date NOT NULL,
+  `judul_buku` varchar(30) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `file_buku` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `laporan`
+--
+
+INSERT INTO `laporan` (`id_buku`, `nama`, `tanggal`, `judul_buku`, `alamat`, `no_hp`, `jenis_kelamin`, `file_buku`) VALUES
+(2, 'Marsha', '2023-05-11', 'Tesis bab 2', 'Surabaya', '0987654321', 'Perempuan', 0x4d616e616a656d656e2050656e67616d62696c616e206b657075747573616e2064616c616d206f7267616e69736173692e70707478);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `majalah`
+--
+
+CREATE TABLE `majalah` (
+  `id_buku` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `tanggal` date NOT NULL,
+  `judul_buku` varchar(30) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `file_buku` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `majalah`
+--
+
+INSERT INTO `majalah` (`id_buku`, `nama`, `tanggal`, `judul_buku`, `alamat`, `no_hp`, `jenis_kelamin`, `file_buku`) VALUES
+(1, 'Akbar', '2023-05-17', 'iNews', 'Depok', '098764567', 'Laki-laki', 0x6a51756572792e70707478);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `naskah`
+--
+
+CREATE TABLE `naskah` (
+  `id_buku` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `tanggal` date NOT NULL,
+  `judul_buku` varchar(30) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `file_buku` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `novel`
+--
+
+CREATE TABLE `novel` (
+  `id_buku` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `tanggal` date NOT NULL,
+  `judul_buku` varchar(30) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `file_buku` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `komik`
+--
+ALTER TABLE `komik`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indexes for table `laporan`
+--
+ALTER TABLE `laporan`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indexes for table `majalah`
+--
+ALTER TABLE `majalah`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indexes for table `naskah`
+--
+ALTER TABLE `naskah`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indexes for table `novel`
+--
+ALTER TABLE `novel`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `komik`
+--
+ALTER TABLE `komik`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `laporan`
+--
+ALTER TABLE `laporan`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `majalah`
+--
+ALTER TABLE `majalah`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `naskah`
+--
+ALTER TABLE `naskah`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `novel`
+--
+ALTER TABLE `novel`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
